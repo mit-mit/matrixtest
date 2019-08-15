adbce997b663a7bca409342e5efc20162de5d82b
Michael Thomsen <mit@google.com>
2019-08-15 16:56:27 +0200

    Template

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:matrixtest/matrixtest.dart' as matrixtest;
+
+main(List<String> arguments) {
+  print('Hello world: ${matrixtest.calculate()}!');
+}
---
+++ test/matrixtest_test.dart
@@ -0,0 +1,8 @@
+import 'package:matrixtest/matrixtest.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
